69679bde0cf3e628480319ea8127175d5b1b7623
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-08-25 02:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let name=document.querySelector("#username");
+let checkbox=document.querySelector("#checkbox");
+let submitBtn=document.querySelector("#submit");
+let existingBtn=document.querySelector("#existing");
+let password=document.querySelector("#password"); 
+
+if(localStorage.getItem("user")!==null){
+	existingBtn.style.display="block";
+}
+existingBtn.addEventListener("click",()=>{
+	let savedUsername=JSON.parse(localStorage.getItem("user")).username;
+	alert(`Logged in as ${savedUsername}`);
+})
+submitBtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let username=name.value;
+	let pass=password.value;
+	
+	alert(`Logged in as ${username}`);
+	if(checkbox.checked){
+		localStorage.setItem("user",JSON.stringify({username,pass}));
+	}
+	else{
+		localStorage.clear();
+	}
+})
